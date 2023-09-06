cb2a35c964df8c8a773ef77b03f94f980790d3a0
Libovski <goncharov199306@gmail.com>
2023-09-06 22:06:40 +0700

    create HashMap array with constants

--- src/RomanToInteger/Converter.java
+++ src/RomanToInteger/Converter.java
@@ -3,6 +3,7 @@ package RomanToInteger;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashMap;
 
 public class Converter {
     public static void main(String[] args) throws IOException {
@@ -11,5 +12,17 @@ public class Converter {
         String romanString = bufferedReader.readLine();
         System.out.println("Conversion result equals " + romanToInteger(romanString));
     }
-    public static int romanToInteger(String s) {return 0;}
+    public static int romanToInteger(String s) {
+        HashMap<String, Integer> mapa = new HashMap<>();
+        mapa.put("I", 1);
+        mapa.put("V", 5);
+        mapa.put("X", 10);
+        mapa.put("L", 50);
+        mapa.put("C", 100);
+        mapa.put("D", 500);
+        mapa.put("M", 1000);
+
+
+        return 0;
+    }
 }
