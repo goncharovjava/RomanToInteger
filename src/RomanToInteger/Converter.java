ca4664be8a35f0408c23a34028b66b3ff8a75866
Libovski <goncharov199306@gmail.com>
2023-09-06 22:08:26 +0700

    add logical part

--- src/RomanToInteger/Converter.java
+++ src/RomanToInteger/Converter.java
@@ -22,7 +22,19 @@ public class Converter {
         mapa.put("D", 500);
         mapa.put("M", 1000);
 
+        int result = 0;
+        int temp = 0;
 
-        return 0;
+        for (int i = 0; i < s.length(); i++) {
+            int temp1 = mapa.get(String.valueOf(s.charAt(i)));
+            if (temp1 > temp){
+                result = result + temp1 - (temp * 2);
+            }else {
+                result += temp1;
+            }
+            temp = temp1;
+        }
+
+        return result;
     }
 }
